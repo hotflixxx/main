65739632a4f2b39ebf94319a6459153a11b58006
hotflixxx <hotflixoficial@gmail.com>
2024-02-27 23:45:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // URL del archivo JSON que contiene la información de los videos
     const jsonUrl = "videos.json";
 
-    // Realiza una solicitud HTTP GET para cargar el JSON
     fetch(jsonUrl)
         .then(response => response.json())
         .then(data => {
-            // Itera sobre los datos del JSON
             data.videos.forEach(video => {
-                // Crea un elemento iframe para cada video
                 const iframe = document.createElement("iframe");
                 iframe.src = video.src;
                 iframe.frameborder = 0;
@@ -16,12 +12,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 iframe.scrolling = "no";
                 iframe.allowfullscreen = true;
 
-                // Calcula el ancho del iframe en función del ancho de la pantalla
                 const screenWidth = window.innerWidth || document.documentElement.clientWidth;
-                const maxWidth = Math.min(560, screenWidth); // Limita el ancho máximo a 560px
+                const maxWidth = Math.min(560, screenWidth);
                 iframe.width = maxWidth;
 
-                // Crea un contenedor para el video y añádelo al DOM
                 const container = document.createElement("div");
                 container.classList.add("container-videos");
                 container.appendChild(iframe);
